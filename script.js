716b9dd94f17e9a0bb8ab7731620f71e46b060c0
Ra'eesah <raeesahgoolamossen@gmail.com>
2026-03-27 11:02:28 +0400

    Updates

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ const closeBtn = document.querySelector('.close-btn');
 //Clickable card like flashcards but in modal format
 flashcards.forEach(card => {
     card.addEventListener('click', () => {
-        // Grab the info from the data attributes I made in HTML
+        //It takes the info from the data attributes and puts it in the popup modal
         modalTitle.textContent = card.getAttribute('data-title');
         modalDesc.textContent = card.getAttribute('data-desc');
         
-        // Show the popup and stop the page from scrolling behind it
+        //The popup modal pops up and it stops the page from scrolling behind it
         modal.style.display = 'flex';
         document.body.style.overflow = 'hidden'; 
     });
@@ -23,20 +23,20 @@ flashcards.forEach(card => {
 //Function to hide the popup again
 const closePopup = () => {
     modal.style.display = 'none';
-    document.body.style.overflow = 'auto'; // Let the page scroll again
+    document.body.style.overflow = 'auto';
 };
 
 //Close when clicking the 'x'
 closeBtn.onclick = closePopup;
 
-//Close if I click anywhere outside the white box
+//It will close the popup whereever i 
 window.onclick = (e) => { 
     if (e.target === modal) closePopup(); 
 };
 
 
 //Scroll animation section
-// To make things fade in when I scroll down to the 
+//To make things fade in when I scroll down  
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         //If the section is visible on screen, add the 'active' class
@@ -44,7 +44,7 @@ const observer = new IntersectionObserver((entries) => {
             entry.target.classList.add('active');
         }
     });
-}, { threshold: 0.1 }); // Trigger when 10% of the section is visible
+}, { threshold: 0.1 }); //Trigger when 10% of the section is visible
 
 //Globalise reveal
 document.querySelectorAll('.reveal').forEach(el => observer.observe(el));

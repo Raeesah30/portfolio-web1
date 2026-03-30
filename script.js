64d36da801364c35b2f9705ff0fd2df578ea5d24
Ra'eesah <raeesahgoolamossen@gmail.com>
2026-03-30 11:08:53 +0400

    added a bottom border for nav

--- script.js
+++ script.js
@@ -13,12 +13,10 @@ navLinks.querySelectorAll('a').forEach(link => {
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
-        //If the section is visible on screen, add the 'active' class
         if (entry.isIntersecting) {
             entry.target.classList.add('active');
         }
     });
-}, { threshold: 0.1 }); //Trigger when 10% of the section is visible
+}, { threshold: 0.1 });
 
-//Globalise reveal
 document.querySelectorAll('.reveal').forEach(el => observer.observe(el));
